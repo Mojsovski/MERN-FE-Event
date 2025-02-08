6a1608e9af5652a0af135ad9f72f87096c22dd0a
Mojsovski <amrattategar30@gmail.com>
2025-02-08 18:31:20 +0700

    add:  setup next auth

--- src/config/env.ts
+++ src/config/env.ts
@@ -1,3 +1,6 @@
-const env = { API_URL: process.env.NEXT_PUBLIC_API_URL };
+const env = {
+  API_URL: process.env.NEXT_PUBLIC_API_URL,
+  AUTH_SECRET: process.env.NEXTAUTH_SECRET,
+};
 
 export default env;
