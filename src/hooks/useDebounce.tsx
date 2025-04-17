6d425e4e90802ff68ad582b1f283ab5bc8634a9a
Mojsovski <amrattategar30@gmail.com>
2025-04-17 21:49:40 +0700

    fix: error deploy vercel

--- src/hooks/useDebounce.tsx
+++ src/hooks/useDebounce.tsx
@@ -1,17 +1,18 @@
 import { useRef } from "react";
 
+type AnyFunction = (...args: unknown[]) => void;
+
 const useDebounce = () => {
   const debounceTimeout = useRef<NodeJS.Timeout | null>(null);
 
-  const debounce = <T extends (...args: any[]) => void>(
-    func: T,
-    delay: number
-  ) => {
-    if (debounceTimeout.current) clearTimeout(debounceTimeout.current);
-    debounceTimeout.current = setTimeout(() => {
-      func();
-      debounceTimeout.current = null;
-    }, delay);
+  const debounce = <T extends AnyFunction>(func: T, delay: number) => {
+    return (...args: Parameters<T>) => {
+      if (debounceTimeout.current) clearTimeout(debounceTimeout.current);
+      debounceTimeout.current = setTimeout(() => {
+        func(...args);
+        debounceTimeout.current = null;
+      }, delay);
+    };
   };
 
   return debounce;
